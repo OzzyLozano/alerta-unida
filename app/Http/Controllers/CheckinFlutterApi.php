a4f8284b6da6b63825e46c4336071465e519c171
Oziel Lozano <oziellozanouwu@gmail.com>
2025-09-08 04:03:26 -0500

    chek in controller corrections

--- app/Http/Controllers/CheckinFlutterApi.php
+++ app/Http/Controllers/CheckinFlutterApi.php
@@ -6,20 +6,17 @@ use App\Models\Checkin;
 use Illuminate\Http\Request;
 
 class CheckinFlutterApi extends Controller {
-    public function storeApi(Request $request) {
+    public function checkIn(Request $request) {
     $checkin = new Checkin();
     $checkin->alert_id = $request->alert_id;
+    $checkin->user_id = $request->user_id;
     $checkin->meeting_point = $request->meeting_point;
     $checkin->are_you_okay = $request->are_you_okay;
-    $checkin->name = $request->name;
-    $checkin->email = $request->email;
     $checkin->save();
 
     return response()->json([
         'message' => 'Check-in registrado correctamente',
         'data' => $checkin
-    ], 201);
+    ], 200);
   }
-
-    //
 }

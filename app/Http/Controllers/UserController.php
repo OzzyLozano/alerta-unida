0aac472c24cdecbad3a1d23b8d30ac1dd283f79f
Oziel Lozano <oziellozanouwu@gmail.com>
2025-09-19 02:08:45 -0500

    pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller {
   public function index() {
-    $users = User::all();
+    $users = User::paginate(15);
     return view('admin.users.index', compact('users'));
   }
 

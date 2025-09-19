4a20f14932a5ab45fe078f03318cce37c70f03c9
Oziel Lozano <oziellozanouwu@gmail.com>
2025-09-19 03:29:37 -0500

    responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}?v={{ filemtime(public_path('css/styles.css')) }}">
     <title>@yield('page-title', 'page')</title>
   </head>
   <body>
